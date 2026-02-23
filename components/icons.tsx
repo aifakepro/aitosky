bf4672bf94b9b31e0869e9e827e207b964eb968e
aifakepro <aifake.pro@gmail.com>
2026-02-23 19:10:50 +0200

    add google oauth env vars

--- components/icons.tsx
+++ components/icons.tsx
@@ -36,7 +36,7 @@ export type Icon = LucideIcon;
 
 export const Icons = {
   dashboard: LayoutDashboardIcon,
-  logo: Slack,
+  logo: () => <img src="/F32.png" alt="logo" width={32} height={32} />,
   login: LogIn,
   close: X,
   profile: UserCog,
