0545c073465fbc84a7de5a547ba850383503e590
aifakepro <aifake.pro@gmail.com>
2026-03-24 16:06:24 +0200

    update

--- components/charts/area-graph.tsx
+++ components/charts/area-graph.tsx
@@ -1,14 +1,8 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { TrendingUp } from 'lucide-react';
-import {
-  Area,
-  AreaChart,
-  CartesianGrid,
-  XAxis,
-  ResponsiveContainer
-} from 'recharts';
+import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
 import {
   Card,
   CardContent,
@@ -23,10 +17,8 @@ import {
   ChartTooltip,
   ChartTooltipContent
 } from '@/components/ui/chart';
-import { revalidatePath } from 'next/cache';
 
-export const description =
-  'An area chart with gradient fill and refined layout';
+export const description = 'An area chart with gradient fill';
 
 const chartData = [
   { month: 'Jan', desktop: 186, mobile: 80 },
@@ -40,33 +32,15 @@ const chartData = [
 const chartConfig = {
   desktop: {
     label: 'Desktop',
-    color: 'hsl(252.12121212121215, 98.01980198019803%, 60.392156862745104%)'
+    color: 'hsl(252, 98%, 60%)'
   },
   mobile: {
     label: 'Mobile',
-    color: 'hsl(172.94117647058823, 90.33816425120776%, 59.411764705882355%)'
+    color: 'hsl(173, 90%, 59%)'
   }
 } satisfies ChartConfig;
 
-const useMediaQuery = (query: any) => {
-  const [matches, setMatches] = React.useState(false);
-
-  React.useEffect(() => {
-    const media = window.matchMedia(query);
-    if (media.matches !== matches) {
-      setMatches(media.matches);
-    }
-    const listener = () => setMatches(media.matches);
-    window.addEventListener('resize', listener);
-    return () => window.removeEventListener('resize', listener);
-  }, [matches, query]);
-
-  return matches;
-};
-
 export function AreaGraph() {
-  const isLargeScreen = useMediaQuery('(min-width: 1024px)');
-  useEffect(() => {}, [isLargeScreen]);
   return (
     <Card className="w-full">
       <CardHeader>
@@ -75,81 +49,80 @@ export function AreaGraph() {
           Showing total visitors for the last 6 months
         </CardDescription>
       </CardHeader>
-      <CardContent>
-        <div className="h-fit md:h-[300px]">
-          <ChartContainer config={chartConfig}>
-            <ResponsiveContainer width="100%" height="100%">
-              <AreaChart
-                accessibilityLayer
-                data={chartData}
-                margin={{
-                  top: 0,
-                  left: 12,
-                  right: 12,
-                  bottom: 0
-                }}
-              >
-                <CartesianGrid vertical={false} />
-                <XAxis
-                  dataKey="month"
-                  tickLine={true}
-                  axisLine={true}
-                  tickMargin={8}
-                  tickFormatter={(value) => value.slice(0, 3)}
+      <CardContent className="px-2 sm:p-6">
+        <ChartContainer
+          config={chartConfig}
+          className="aspect-auto h-[310px] w-full"
+        >
+          <AreaChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              top: 12,
+              left: 12,
+              right: 12,
+              bottom: 0
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="dot" />}
+            />
+            <defs>
+              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor="var(--color-desktop)"
+                  stopOpacity={0.8}
                 />
-                <ChartTooltip
-                  cursor={false}
-                  content={<ChartTooltipContent />}
+                <stop
+                  offset="95%"
+                  stopColor="var(--color-desktop)"
+                  stopOpacity={0.1}
                 />
-                <defs>
-                  <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
-                    <stop
-                      offset="5%"
-                      stopColor="var(--color-desktop)"
-                      stopOpacity={0.8}
-                    />
-                    <stop
-                      offset="95%"
-                      stopColor="var(--color-desktop)"
-                      stopOpacity={0.1}
-                    />
-                  </linearGradient>
-                  <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
-                    <stop
-                      offset="5%"
-                      stopColor="var(--color-mobile)"
-                      stopOpacity={0.8}
-                    />
-                    <stop
-                      offset="95%"
-                      stopColor="var(--color-mobile)"
-                      stopOpacity={0.1}
-                    />
-                  </linearGradient>
-                </defs>
-                <Area
-                  dataKey="mobile"
-                  type="natural"
-                  fill="url(#fillMobile)"
-                  fillOpacity={0.4}
-                  stroke="var(--color-mobile)"
-                  stackId="a"
+              </linearGradient>
+              <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor="var(--color-mobile)"
+                  stopOpacity={0.8}
                 />
-                <Area
-                  dataKey="desktop"
-                  type="natural"
-                  fill="url(#fillDesktop)"
-                  fillOpacity={0.4}
-                  stroke="var(--color-desktop)"
-                  stackId="a"
+                <stop
+                  offset="95%"
+                  stopColor="var(--color-mobile)"
+                  stopOpacity={0.1}
                 />
-              </AreaChart>
-            </ResponsiveContainer>
-          </ChartContainer>
-        </div>
+              </linearGradient>
+            </defs>
+            <Area
+              dataKey="mobile"
+              type="natural"
+              fill="url(#fillMobile)"
+              fillOpacity={0.4}
+              stroke="var(--color-mobile)"
+              stackId="a"
+            />
+            <Area
+              dataKey="desktop"
+              type="natural"
+              fill="url(#fillDesktop)"
+              fillOpacity={0.4}
+              stroke="var(--color-desktop)"
+              stackId="a"
+            />
+          </AreaChart>
+        </ChartContainer>
       </CardContent>
       <CardFooter className="flex flex-col items-start pt-4">
-        <div className="flex items-center gap-2 text-sm font-medium">
+        <div className="flex items-center gap-2 text-sm font-medium leading-none">
           Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
         </div>
         <div className="text-sm text-muted-foreground">January - June 2024</div>
