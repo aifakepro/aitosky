0c3b9f365654a04a903aa368e41ebbafecf93a78
aifakepro <aifake.pro@gmail.com>
2026-03-25 15:38:05 +0200

    update

--- auth.ts
+++ auth.ts
@@ -33,7 +33,7 @@ export const { auth, handlers, signOut, signIn } = NextAuth({
             const d = new Date(today);
             d.setMonth(today.getMonth() + i);
             const month = d.toISOString().slice(0, 7); // "2024-01"
-            return { month, desktop: 5, mobile: 5, userId };
+            return { month, desktop: 5, mobile: 1, userId };
           });
           await prisma.dashboardAreaChart.createMany({ data: areaData });
 
