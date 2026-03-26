e20809387350d94ad106f98c7b2a10377eebe6cd
aifakepro <aifake.pro@gmail.com>
2026-03-26 17:27:14 +0200

    update

--- app/api/kanban/Task/route.ts
+++ app/api/kanban/Task/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 import { Prisma } from '@prisma/client';
+export const revalidate = 0;
 
 export async function POST(req: Request) {
   try {
