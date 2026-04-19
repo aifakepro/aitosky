fb7be9a41a0ced8fc06428adbf92ff8bcea49c19
aifakepro <aifake.pro@gmail.com>
2026-04-19 11:49:28 +0300

    update

--- components/forms/user-auth-form.tsx
+++ components/forms/user-auth-form.tsx
@@ -36,7 +36,6 @@ const registerSchema = z
   });
 
 type SignInFormValue = z.infer<typeof signInSchema>;
-type RegisterFormValue = z.infer<typeof registerSchema>;
 type UserFormValue = SignInFormValue & { confirmPassword?: string };
 
 export default function UserAuthForm() {
